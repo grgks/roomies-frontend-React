c6eda04463ea153d66def87ce418b0fc5e2ae31e
KOYN <gkeo933@gmail.com>
2026-06-20 12:08:57 +0300

    feat(components):add RoomiesLogo

---
+++ src/components/RoomiesLogo.tsx
@@ -0,0 +1,14 @@
+const RoomiesLogo = () => {
+    return (
+        <>
+            <img
+                className="h-20 w-45 rounded-sm shadow-lg m-2 shrink-0"
+                src="/roomies.png"
+                alt="Roomies"
+                style={{ minWidth: '180px',  mixBlendMode: 'screen' }}
+            />
+        </>
+    )
+}
+
+export default RoomiesLogo;
