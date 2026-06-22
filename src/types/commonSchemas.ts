6231a1c68737c0b2c1237bd0883f3aa9c3c72675
KOYN <gkeo933@gmail.com>
2026-06-23 00:04:48 +0300

    feat(types):add PaginationParams z.object to commonSchemas

--- src/types/commonSchemas.ts
+++ src/types/commonSchemas.ts
@@ -1,5 +1,12 @@
+import { z } from 'zod';
 
-
+export const PaginationParams = z.object({
+    page: z.number().int().optional(),
+    pageSize: z.number().int().optional(),
+    sortDirection: z.enum(['ASC', 'DESC']).optional(),
+    sortBy: z.string().optional(),
+});
+export type PaginationParams = z.infer<typeof PaginationParams>;
 
 // zod schemas not support easy generics.Interface is natural choice for generic structures.  paginated data
 export interface PagedResponse<T> {
