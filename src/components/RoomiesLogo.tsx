7e294369aced4eb99d51bff6bf1751611f545d93
KOYN <gkeo933@gmail.com>
2026-07-09 16:31:47 +0300

    Resize logo to match displayed aspect ratio

--- src/components/RoomiesLogo.tsx
+++ src/components/RoomiesLogo.tsx
@@ -2,7 +2,7 @@ const RoomiesLogo = () => {
     return (
         <>
             <img
-                className="h-20 w-45 rounded-md shadow-lg m-2 shrink-0"
+                className="h-20 w-[140px] rounded-md shadow-lg m-2 shrink-0"
                 src="/roomies.png"
                 alt="Roomies"
                 style={{ minWidth: '180px',  mixBlendMode: 'screen' }}
